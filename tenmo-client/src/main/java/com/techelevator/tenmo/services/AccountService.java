fd8f2429644b4275600a1e8aef5092f6af6fd2cb
Ryan Godman <rgodman0038@gmail.com>
2021-07-02 19:26:08 -0400

    minor addition to accounts and an empty TransferService class

--- tenmo-client/src/main/java/com/techelevator/tenmo/services/AccountService.java
+++ tenmo-client/src/main/java/com/techelevator/tenmo/services/AccountService.java
@@ -2,11 +2,14 @@ package com.techelevator.tenmo.services;
 
 import com.techelevator.tenmo.model.Account;
 import com.techelevator.tenmo.model.AuthenticatedUser;
+import org.apiguardian.api.API;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
 
+import javax.security.auth.login.AccountNotFoundException;
 import java.math.BigDecimal;
 
 public class AccountService {
@@ -22,6 +25,16 @@ public class AccountService {
     public Account getAccount(AuthenticatedUser currentUser) {
         Account account = null;
         return restTemplate.exchange(API_BASE_URL + "balance/" + currentUser.getUser().getId(), HttpMethod.GET, makeAuthEntity(currentUser), Account.class).getBody();
+    }
+
+    public Account update(AuthenticatedUser currentUser) throws AccountServiceException{
+        Account account = null;
+        try {
+            restTemplate.put(API_BASE_URL + "balance/" + currentUser.getUser().getId(), HttpMethod.PUT, makeAuthEntity(currentUser), Account.class);
+        } catch (RestClientResponseException ex) {
+            throw new AccountServiceException(ex.getRawStatusCode() + " : " + ex.getResponseBodyAsString());
+        }
+        return null;
     }
 
     private HttpEntity makeAuthEntity(AuthenticatedUser currentUser) {
